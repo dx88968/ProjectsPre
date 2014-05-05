82a8041ea1bc0b7d265a0d8ae26b0b5c8416b3f5
XUN XU <dx88968@gmail.com>
2014-05-05 01:02:48 -0400

    simple update

--- scripts/myjs.js
+++ scripts/myjs.js
@@ -3,16 +3,19 @@ $(document).ready(function(){
     $("#tag3").css("background-color","rgb(120,120,120)");
     $("#tag4").css("background-color","rgb(120,120,120)");
     $("#tag5").css("background-color","rgb(120,120,120)");
+    $("#tag6").css("background-color","rgb(120,120,120)");
     $("#tag1").click(function(){
         $("#tag1").css("background-color","#FFFFFF");
         $("#tag2").css("background-color","rgb(120,120,120)");
         $("#tag3").css("background-color","rgb(120,120,120)");
         $("#tag4").css("background-color","rgb(120,120,120)");
         $("#tag5").css("background-color","rgb(120,120,120)");
+        $("#tag6").css("background-color","rgb(120,120,120)");
         $("#digitalsales").css("display","none");
         $("#eba").css("display","none");
         $("#xxsearcher").css("display","none");
-        $("#zookeeper").css("display","none");       
+        $("#zookeeper").css("display","none");     
+        $("#recent").css("display","none");    
         $("#sharemi").show(400);
     });
     $("#tag2").click(function(){
@@ -21,10 +24,12 @@ $(document).ready(function(){
         $("#tag3").css("background-color","rgb(120,120,120)");
         $("#tag4").css("background-color","rgb(120,120,120)");
         $("#tag5").css("background-color","rgb(120,120,120)");
+        $("#tag6").css("background-color","rgb(120,120,120)");
         $("#eba").css("display","none");
         $("#xxsearcher").css("display","none");
         $("#zookeeper").css("display","none");   
         $("#sharemi").css("display","none");
+        $("#recent").css("display","none");    
         $("#digitalsales").show(400);
     });
     $("#tag3").click(function(){
@@ -33,10 +38,12 @@ $(document).ready(function(){
         $("#tag1").css("background-color","rgb(120,120,120)");
         $("#tag4").css("background-color","rgb(120,120,120)");
         $("#tag5").css("background-color","rgb(120,120,120)");
+        $("#tag6").css("background-color","rgb(120,120,120)");
         $("#xxsearcher").css("display","none");
         $("#zookeeper").css("display","none");       
         $("#sharemi").css("display","none");
         $("#digitalsales").css("display","none");
+        $("#recent").css("display","none");    
         $("#eba").show(400);
     });
     $("#tag4").click(function(){
@@ -45,10 +52,12 @@ $(document).ready(function(){
         $("#tag1").css("background-color","rgb(120,120,120)");
         $("#tag3").css("background-color","rgb(120,120,120)");
         $("#tag5").css("background-color","rgb(120,120,120)");
+        $("#tag6").css("background-color","rgb(120,120,120)");
         $("#zookeeper").css("display","none");      
         $("#sharemi").css("display","none");
         $("#digitalsales").css("display","none");
         $("#eba").css("display","none");
+        $("#recent").css("display","none");    
         $("#xxsearcher").show(400);
     });
     $("#tag5").click(function(){
@@ -57,10 +66,26 @@ $(document).ready(function(){
         $("#tag1").css("background-color","rgb(120,120,120)");
         $("#tag3").css("background-color","rgb(120,120,120)");
         $("#tag4").css("background-color","rgb(120,120,120)");
+        $("#tag6").css("background-color","rgb(120,120,120)");
         $("#sharemi").css("display","none");
         $("#digitalsales").css("display","none");
         $("#eba").css("display","none");
         $("#xxsearcher").css("display","none");
+        $("#recent").css("display","none");    
         $("#zookeeper").show(400);  
+    });
+    $("#tag6").click(function(){
+        $("#tag6").css("background-color","#FFFFFF");
+        $("#tag2").css("background-color","rgb(120,120,120)");
+        $("#tag1").css("background-color","rgb(120,120,120)");
+        $("#tag3").css("background-color","rgb(120,120,120)");
+        $("#tag4").css("background-color","rgb(120,120,120)");
+        $("#tag5").css("background-color","rgb(120,120,120)");
+        $("#sharemi").css("display","none");
+        $("#digitalsales").css("display","none");
+        $("#eba").css("display","none");
+        $("#xxsearcher").css("display","none");
+        $("#zookeeper").css("display","none");    
+        $("#recent").show(400);  
     });
 });
